b6303de2599449ad2ef2dd04a1ae8995084bdb9e
Bhavya Sri <24r21a6767@mlrit.ac.in>
2026-08-17 18:35:43 +0530

    Added insert+begin_end

---
+++ insert_B_E.java
@@ -0,0 +1,48 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public class insert_B_E{
+    static void printll(Node head){
+        Node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
+    static Node  insertAtBeginning(int x,Node head){
+        Node newNode=new Node(x);
+        newNode.next=head;
+        head=newNode;
+        return head;
+    }
+    static Node insertAtEnd(int x,Node head){
+        Node newNode=new Node(x);
+        if(head==null){
+            return newNode;
+        }
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newNode;
+        return head;
+    }
+    public static void main(String args[]){
+        Node head=new Node(10);
+        head.next=new  Node(20);
+        head.next.next=new Node(30);
+        System.out.println("Before  insert at beginning");
+        printll(head);
+        System.out.println("After inserton");
+        head=insertAtBeginning(5,head);
+        printll(head);
+        System.out.println("After insert at end");
+        head=insertAtEnd(40,head);
+        printll(head);
+    }
+}
